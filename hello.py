4515608dce2bcb23b02ac36176733b8e9f0ee305
tiagoscristovao <tiagoscristovao@outlook.com>
2023-10-18 21:50:19 +0000

    arquivo hello.py alterado em dev

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
 print('Olá Projeto!! Vamos nos dar bem com Python!! Obrigado EBAC.)
 print('Vamos modificar para aprender sobre o GITHUB')
 print('Exercicio Mod18')
+print('Estamos no Mod 19, trabalhando com as branchs')
